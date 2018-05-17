ecad39203c6c55d6ba2fbe173ffbf8bcfcb29309
John Cinq-Mars <jcinqmars2000@yahoo.com>
2018-05-17 13:06:56 -0400

    updtaed

--- src/main/java/guru/springframework/controllers/MyController.java
+++ src/main/java/guru/springframework/controllers/MyController.java
@@ -2,15 +2,21 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import guru.springframework.services.GreetingService;
+
 /**
  * Created by jt on 5/23/17.
  */
 @Controller
 public class MyController {
+	private GreetingService greetingService;
 
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
     public String hello(){
         System.out.println("Hello!!! ");
 
-        return "foo";
+        return greetingService.sayGreeting();
     }
 }
